2e067f24d72b5e5dd891c13335c1d06915b26b9c
developer <andrewkrupenko338@gmail.com>
2023-01-20 19:30:45 +0200

    implemented simple multistep functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useMultistepForm } from "./hooks/useMultistepForm";
+
 function App() {
+  const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } =
+    useMultistepForm([<div>One</div>, <div>Two</div>, <div>Three</div>]);
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <form>
+        <div className="form-header">
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div className="form-controls">
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {isLastStep ? "Finish" : "Next"}
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
